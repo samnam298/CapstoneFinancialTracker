edc85b3e839248276e46a92a26335bf2b49b89f3
samnam298 <samrawithailena@gmail.com>
2025-10-14 10:39:21 -0400

    Added Transaction class to store transaction data

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() { return date; }
+    public LocalTime getTime() { return time; }
+    public String getDescription() { return description; }
+    public String getVendor() { return vendor; }
+    public double getAmount() { return amount; }
+
+    @Override
+    public String toString() {
+        return String.format("%s | %s | %-20s | %-10s | %10.2f",
+                date, time, description, vendor, amount);
+    }
+}
